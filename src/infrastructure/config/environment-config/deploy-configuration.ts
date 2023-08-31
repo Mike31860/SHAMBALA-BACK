2d191e94eb4551b9b2a1df2bbe47cdd8b1f905dc
Mike31860 <miguel980429@gmail.com>
2023-08-31 08:08:48 -0400

    updated support for reading env variables

--- src/infrastructure/config/environment-config/deploy-configuration.ts
+++ src/infrastructure/config/environment-config/deploy-configuration.ts
@@ -1,12 +1,55 @@
-import { readFileSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
 
 const YAML_CONFIG_FILENAME = '.env.local.yaml';
 
-export default () => {
-  return yaml.load(readFileSync(join(YAML_CONFIG_FILENAME), 'utf8')) as Record<
-    string,
-    any
-  >;
+export interface Configurations {
+  app: {
+    port: number;
+    'context-path': string;
+  };
+  db: {
+    user: string;
+    password: string;
+    host: string;
+    name: string;
+    port?: number;
+    schema?: string;
+    sync: boolean;
+  };
+  firebase: {
+    project_id: string;
+    private_key: string;
+    client_email: string;
+  };
+}
+export const configurations = (): Partial<Configurations> => {
+  const prodEnv = process.env.ENV;
+  console.log('ENVVV ', prodEnv);
+  if (prodEnv == 'prod' || !existsSync(join(YAML_CONFIG_FILENAME))) {
+    const configurations: Partial<Configurations> = {
+      app: {
+        'context-path': process.env.APP_CONTEXT_PATH,
+        port: parseInt(process.env.APP_PORT ?? '3000'),
+      },
+      db: {
+        host: process.env.DB_HOST,
+        name: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        sync: new Boolean(process.env.DB_SYNC).valueOf(),
+        user: process.env.DB_USER,
+      },
+      firebase: {
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY,
+      },
+    };
+    console.log('CONFIGS ', configurations);
+    return configurations;
+  }
+  return yaml.load(
+    readFileSync(join(YAML_CONFIG_FILENAME), 'utf8'),
+  ) as Configurations;
 };
--- src/infrastructure/config/environment-config/models.ts
+++ src/infrastructure/config/environment-config/models.ts
@@ -1,5 +1,7 @@
+import { ServiceAccount } from 'firebase-admin';
+
 export interface AppConfigurations {
   getAppPort(): number;
   getAppContextPath(): string;
-  getFirebaseServiceAccountPath(): string;
+  getFirebaseServiceAccountPath(): ServiceAccount;
 }
